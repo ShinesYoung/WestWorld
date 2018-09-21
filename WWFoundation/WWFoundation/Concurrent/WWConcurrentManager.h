ef83e00e11f82fedbbdc1abd19ebed70e0535e09
yangshansi176 <yangshansi176@pingan.com.cn>
2018-09-21 13:37:47 +0800

    添加多线程服务控制器

---
+++ WWFoundation/WWFoundation/Concurrent/WWConcurrentManager.h
@@ -0,0 +1,15 @@
+//
+//  WWConcurrentManager.h
+//  WWFoundation
+//
+//  Created by 杨善嗣 on 2018/9/21.
+//  Copyright © 2018年 net.shines. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WWConcurrentManager : NSObject
+
++ (instancetype)instance;
+
+@end
---
+++ WWFoundation/WWFoundation/Concurrent/WWConcurrentManager.m
@@ -0,0 +1,19 @@
+//
+//  WWConcurrentManager.m
+//  WWFoundation
+//
+//  Created by 杨善嗣 on 2018/9/21.
+//  Copyright © 2018年 net.shines. All rights reserved.
+//
+
+#import "WWConcurrentManager.h"
+
+@implementation WWConcurrentManager
+
+
+
+/******************************************************************************/
+/**** System - Lifecycle & Constructor                                     ****/
+/******************************************************************************/
+#pragma mark - System - Lifecycle & Constructor
+@end
