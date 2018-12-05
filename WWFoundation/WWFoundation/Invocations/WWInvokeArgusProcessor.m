b71ff9e41688cfa9a5e6e815ddce4136831f54c5
Shines Young <yss_1984@163.com>
2018-12-05 23:40:20 +0800

    修复index坐标偏移的问题

--- WWFoundation/WWFoundation/Invocations/WWInvokeArgusProcessor.m
+++ WWFoundation/WWFoundation/Invocations/WWInvokeArgusProcessor.m
@@ -30,71 +30,71 @@
         else if (strcmp(arguCType, @encode(BOOL) ) == 0) // bool
         {
             BOOL boolArg = [numberArg boolValue];
-            [aInvocation setArgument:&boolArg atIndex:index+2];
+            [aInvocation setArgument:&boolArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(float) ) == 0)
         {
             float floatArg = [numberArg floatValue];
-            [aInvocation setArgument:&floatArg atIndex:index+2];
+            [aInvocation setArgument:&floatArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(double) ) == 0)
         {
             double doubleArg = [numberArg doubleValue];
-            [aInvocation setArgument:&doubleArg atIndex:index+2];
+            [aInvocation setArgument:&doubleArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(int) ) == 0)
         {
             int intArg = [numberArg intValue];
-            [aInvocation setArgument:&intArg atIndex:index+2];
+            [aInvocation setArgument:&intArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(unsigned int) ) == 0)
         {
             unsigned int unsignedIntArg = [numberArg unsignedIntValue];
-            [aInvocation setArgument:&unsignedIntArg atIndex:index+2];
+            [aInvocation setArgument:&unsignedIntArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(long) ) == 0)
         {
             long longArg = [numberArg longValue];
-            [aInvocation setArgument:&longArg atIndex:index+2];
+            [aInvocation setArgument:&longArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(unsigned long) ) == 0)
         {
             unsigned long unsignedLongArg = [numberArg unsignedLongValue];
-            [aInvocation setArgument:&unsignedLongArg atIndex:index+2];
+            [aInvocation setArgument:&unsignedLongArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(long long) ) == 0)
         {
             long long longLongArg = [numberArg longLongValue];
-            [aInvocation setArgument:&longLongArg atIndex:index+2];
+            [aInvocation setArgument:&longLongArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(unsigned long long) ) == 0)
         {
             unsigned long long unsignedLongLongArg = [numberArg unsignedLongLongValue];
-            [aInvocation setArgument:&unsignedLongLongArg atIndex:index+2];
+            [aInvocation setArgument:&unsignedLongLongArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(char) ) == 0)
         {
             char charArg = [numberArg charValue];
-            [aInvocation setArgument:&charArg atIndex:index+2];
+            [aInvocation setArgument:&charArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(unsigned char) ) == 0)
         {
             unsigned char unsignedCharArg = [numberArg unsignedCharValue];
-            [aInvocation setArgument:&unsignedCharArg atIndex:index+2];
+            [aInvocation setArgument:&unsignedCharArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(short) ) == 0)
         {
             short shortArg = [numberArg shortValue];
-            [aInvocation setArgument:&shortArg atIndex:index+2];
+            [aInvocation setArgument:&shortArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(unsigned short) ) == 0)
         {
             unsigned short unsignedShortArg = [numberArg unsignedShortValue];
-            [aInvocation setArgument:&unsignedShortArg atIndex:index+2];
+            [aInvocation setArgument:&unsignedShortArg atIndex:index];
         }
         else
         {
-            [aInvocation setArgument:&aArgument atIndex:index+2];
+            [aInvocation setArgument:&aArgument atIndex:index];
         }
         
     }
@@ -104,37 +104,37 @@
         if      (strcmp(arguCType, @encode(CGPoint)) == 0)
         {
             CGPoint cgPointArg = [aValueArg CGPointValue];
-            [aInvocation setArgument:&cgPointArg atIndex:index+2];
+            [aInvocation setArgument:&cgPointArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(CGSize)) == 0)
         {
             CGSize cgSizeArg = [aValueArg CGSizeValue];
-            [aInvocation setArgument:&cgSizeArg atIndex:index+2];
+            [aInvocation setArgument:&cgSizeArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(CGVector)) == 0)
         {
             CGVector cgVectorArg = [aValueArg CGVectorValue];
-            [aInvocation setArgument:&cgVectorArg atIndex:index+2];
+            [aInvocation setArgument:&cgVectorArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(CGRect)) == 0)
         {
             CGRect cgRectArg = [aValueArg CGRectValue];
-            [aInvocation setArgument:&cgRectArg atIndex:index+2];
+            [aInvocation setArgument:&cgRectArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(NSRange)) == 0)
         {
             NSRange rangeArg = [aValueArg rangeValue];
-            [aInvocation setArgument:&rangeArg atIndex:index+2];
+            [aInvocation setArgument:&rangeArg atIndex:index];
         }
         else if (strcmp(arguCType, @encode(UIOffset)) == 0)
         {
             UIOffset offsetArg = [aValueArg UIOffsetValue];
-            [aInvocation setArgument:&offsetArg atIndex:index+2];
+            [aInvocation setArgument:&offsetArg atIndex:index];
         }
     }
     else
     {
-        [aInvocation setArgument:&aArgument atIndex:index+2];
+        [aInvocation setArgument:&aArgument atIndex:index];
     }
 }
 
